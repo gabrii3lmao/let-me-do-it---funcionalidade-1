7323331bc166052e7fd86e8beff488d8f63e2de6
gabrii3l <gabrielmluz56@gmail.com>
2025-10-26 14:34:40 -0300

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative 'escola_class'
+
+
+ifpi = Escola.new
+
+# ifpi.adicionar_aluno_em_turma("Informática", "gabriel", 17)
+# ifpi.adicionar_aluno_em_turma("Informática", "maria", 16)
+
+# turma = ifpi.turmas["Informática"]
+# turma.alunos.first.adicionar_nota(8)
+# turma.alunos.first.adicionar_nota(10)
+# turma.alunos.last.adicionar_nota(7)
+# turma.alunos.last.adicionar_nota(5)
+# ifpi.mostrar_alunos_turma("Informática")
+
+# ifpi.salvar_dados
